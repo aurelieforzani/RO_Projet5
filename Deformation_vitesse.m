d621ec9e2cb3203ad315af51ad3d63039d85b22d
Forzani Aurelie <aforzani@localhost.localdomain>
2018-01-12 17:08:24 +0100

    resolution erreur

--- Deformation_vitesse.m
+++ Deformation_vitesse.m
@@ -1,7 +1,4 @@
 detect
-
-T(1).vitesse <= vmax && size(objets_heurtes,2)>0
-
 while(T(1).vitesse > 0 && size(objets_heurtes,2)>0)
     T(1).vitesse = T(1).vitesse - 0.1;
     save(NOMFIC,'im','nombre_objets','subd','T');
@@ -9,10 +6,10 @@ while(T(1).vitesse > 0 && size(objets_heurtes,2)>0)
     detect
 end
 
-if (T(1).vitesse == 0)
+if (T(1).vitesse <= 0)
     disp('pas de vitesse trouvée oour cette trajectoire');
 else 
-    disp("on a choisit une vitesse de" , T(1).vitesse);
+    disp(["on a choisit une vitesse de" , T(1).vitesse]);
 end
 
     
