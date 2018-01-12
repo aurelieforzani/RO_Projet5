cf19bfb5180b8499b77d420d3cec201be4a1f3f2
Forzani Aurelie <aforzani@localhost.localdomain>
2018-01-12 16:47:20 +0100

    ...

--- Deformation_vitesse.m
+++ Deformation_vitesse.m
@@ -1,20 +1,18 @@
-
-vmax = 10;
-T(1).vitesse=1;
-save(NOMFIC,'im','nombre_objets','subd','T');
-
 detect
 
 T(1).vitesse <= vmax && size(objets_heurtes,2)>0
 
-while(T(1).vitesse <= vmax && size(objets_heurtes,2)>0)
-    T(1).vitesse = T(1).vitesse + 0.5;
+while(T(1).vitesse > 0 && size(objets_heurtes,2)>0)
+    T(1).vitesse = T(1).vitesse - 0.1;
     save(NOMFIC,'im','nombre_objets','subd','T');
     
     detect
-    
-    objets_heurtes
 end
 
-T.vitesse
+if (T(1).vitesse == 0)
+    disp('pas de vitesse trouvée oour cette trajectoire');
+else 
+    disp("on a choisit une vitesse de" , T(1).vitesse);
+end
+
     
