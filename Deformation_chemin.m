bc5dcbb545d5db6390a866aef1bb2709dc247c47
Forzani Aurelie <aforzani@localhost.localdomain>
2018-01-16 13:03:26 +0100

    fini git add Deformation_chemin.m !

--- Deformation_chemin.m
+++ Deformation_chemin.m
@@ -1,9 +1,10 @@
 global position
 
-detect
 
-if ( size(objets_heurtes,2)>0 )
-    T(1).nurbs.coefs
+while(T(1).nurbs.coefs(4,indicePointControle) > 0 && size(objets_heurtes,2)>0)
+    
+    detect
+    
     distance = norm(position - T(1).nurbs.coefs(1:2,1)./T(1).nurbs.coefs(4,1));
     for (i = 2 : size(T(1).nurbs.coefs,2))
         distanceNouvelle = norm(position -  T(1).nurbs.coefs(1:2,i)./T(1).nurbs.coefs(4,i));
@@ -12,14 +13,9 @@ if ( size(objets_heurtes,2)>0 )
             indicePointControle = i;
         end
     end
-end
-
-while(T(1).nurbs.coefs(4,indicePointControle) > 0 && size(objets_heurtes,2)>0)
-    
     T(1).nurbs.coefs(4,indicePointControle) = T(1).nurbs.coefs(4,indicePointControle)*0.9;
     save(NOMFIC,'im','nombre_objets','subd','T');
     
-    detect
 end
 
 
